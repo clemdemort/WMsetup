624ea326f7cda483ec05f95e6b50eb94c738feb4
clemdemort <62178977+clemdemort@users.noreply.github.com>
2022-08-02 15:41:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,6 +48,7 @@ cd $path
 sudo mv xinitrc /etc/X11/xinit/xinitrc
 
 echo "modifying vim"
+sudo rm ~/.vim_runtime -r
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
