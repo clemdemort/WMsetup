abe0309eb63e4ab1fd27d8ee7b4b3a8ff217b5b4
clemdemort <62178977+clemdemort@users.noreply.github.com>
2022-08-04 23:10:55 +0200

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -71,12 +71,13 @@ static const char *light_down[] = {"xbacklight","-inc","-10", NULL};
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *scrshot[]  = { "flameshot","gui", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,		        XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,		                XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_Up,     incnmaster,     {.i = +1 } },
@@ -112,6 +113,8 @@ static Key keys[] = {
 	{ 0, 				XF86XK_AudioRaiseVolume,  spawn, {.v = upvol} },
 	{ 0,				XF86XK_MonBrightnessUp,		spawn,	{.v = light_up} },
 	{ 0,				XF86XK_MonBrightnessDown,	spawn,	{.v = light_down} },
+    { 0,		                XK_Print,  spawn,          {.v = scrshot } },
+
 };
 
 /* button definitions */
